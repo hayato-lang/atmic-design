d278251974db36b05ff5f26d48a2c8d8658f5cfc
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-20 18:43:59 +0900

    Templateの作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SeconderyButton } from "./components/atoms/button/SeconderyButton";
 import { SearchInput } from "./components/molcules/SearchInput";
 import { UserCard } from "./components/organism/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
 import "./styles.css";
 
 const user = {
@@ -18,12 +19,14 @@ const user = {
 
 export default function App() {
   return (
-    <HeaderOnly>
-      <PrimaryButton>テスト</PrimaryButton>
-      <SeconderyButton>検索</SeconderyButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </HeaderOnly>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SeconderyButton>検索</SeconderyButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
--- src/components/atoms/layout/Header.jsx
+++ src/components/atoms/layout/Header.jsx
@@ -1,1 +1,22 @@
-export const Header = () => {};
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Header = () => {
+  return (
+    <SHeader>
+      <SLink to="/">HOME</SLink>
+      <SLink to="/">USERS</SLink>
+    </SHeader>
+  );
+};
+
+const SHeader = styled.header`
+  background-color: #11999e;
+  color: #fff;
+  text-align: center;
+  padding: 8px 0;
+`;
+
+const SLink = styled(Link)`
+  margin: 0 8px;
+`;
