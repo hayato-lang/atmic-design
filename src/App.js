47ffe239568d7b8547a94643e149cf5fa624a7d8
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-22 07:49:17 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "勇人",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "dddd@ddd.com",
-  phone: "080-1234-5678",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
