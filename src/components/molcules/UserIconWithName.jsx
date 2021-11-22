00098f23d97801fe56c1cd123c4515ee1b4c45b3
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-22 10:01:47 +0900

    手間のかかるやり方

--- src/components/molcules/UserIconWithName.jsx
+++ src/components/molcules/UserIconWithName.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 export const UserIconWithName = (props) => {
-  const { name, image } = props;
+  const { name, image, isAdmin } = props;
   return (
     <SContainer>
-      <SImage height={160} width={160} src={image} alt={name} />
+      <SImage
+        height={160}
+        width={160}
+        src={image}
+        alt={name}
+        isAdmin={isAdmin}
+      />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
 };
@@ -23,4 +30,10 @@ const SName = styled.p`
   margin: 0;
   font-size: 18px;
   color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  color: #aaa;
 `;
--- src/components/page/Top.jsx
+++ src/components/page/Top.jsx
@@ -1,9 +1,23 @@
+import React from "react";
 import styled from "styled-components";
+import { SeconderyButton } from "../atoms/button/SeconderyButton";
+import { useHistory } from "react-router-dom";
 
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () => {
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  };
+  const onClickGeneral = () => {
+    history.push({ pathname: "/users", state: { isAdmin: false } });
+  };
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SeconderyButton onClick={onClickAdmin}>管理者ユーザー</SeconderyButton>
+      <br />
+      <br />
+      <SeconderyButton onClick={onClickGeneral}>一般ユーザー</SeconderyButton>
     </SContainer>
   );
 };
